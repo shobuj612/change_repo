2f959ca0afad465c3004863cc981c093470e8df3
shobuj612 <iqramulislam55@gmail.com>
2025-05-19 14:28:33 +0600

    dfdfdfdd

--- src/app/Model/order.model.ts
+++ src/app/Model/order.model.ts
@@ -1,6 +1,6 @@
 export class Order {
   order_id?: number; // this is optional 
-  buyer_id: number;
+  buyer: {buyerId:number};
   style_no: string;
   order_qty: number;
   order_date: Date;
@@ -9,7 +9,7 @@ export class Order {
 
   constructor() {
            // Initializing order_id as a number
-    this.buyer_id = 0;       // Initializing buyer_id as a number
+    this.buyer = {buyerId:0};       // Initializing buyer_id as a number
     this.style_no = '';
     this.order_qty = 0;
     this.order_date = new Date();
--- src/app/Model/warehouse.model.ts
+++ src/app/Model/warehouse.model.ts
@@ -1,12 +1,12 @@
 export class Warehouse {
     warehouse_id?: number;
-    order_id: number;
+    order:{order_id:number};
     received_date: string;
     stored_qty: number;
   
     constructor() {
      // this.warehouse_id = 0;
-      this.order_id = 0;
+      this.order = {order_id:0};
       this.received_date = '';
       this.stored_qty = 0;
     }
